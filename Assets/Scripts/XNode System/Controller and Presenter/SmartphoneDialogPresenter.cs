bf46b1a0f028ab2d0d14a1a0f49fd403378125d5
Nicolias <marat.khakimov.04@gmail.com>
2023-05-13 22:05:43 +0500

    Set up new messege indication logic

--- Assets/Scripts/XNode System/Controller and Presenter/SmartphoneDialogPresenter.cs
+++ Assets/Scripts/XNode System/Controller and Presenter/SmartphoneDialogPresenter.cs
@@ -5,17 +5,17 @@ public class SmartphoneDialogPresenter : IPresentar
     public event Action OnComplete;
 
     private NewDialogInSmartphoneModel _model;
-    //private Smartphone _view;
+    private Smartphone _view;
 
-    //public SmartphoneDialogPresenter(NewDialogInSmartphoneModel model, Smartphone view)
-    //{
-    //    _model = model;
-    //    _view = view;
-    //}
+    public SmartphoneDialogPresenter(NewDialogInSmartphoneModel model, Smartphone view)
+    {
+        _model = model;
+        _view = view;
+    }
 
     public void Execute()
     {
-        //_view.AddNewMessege(_model.NewDialog);
+        _view.AddNewMessege(_model.NewDialog);
         OnComplete?.Invoke();
     }
 }
--- Assets/Scripts/XNode System/View/Smartphone/MessegeIndicator.cs
+++ Assets/Scripts/XNode System/View/Smartphone/MessegeIndicator.cs
@@ -1,21 +1,35 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class MessegeIndicator : MonoBehaviour
 {
-    //[SerializeField] private Smartphone _smartphone;
+    [SerializeField] private Smartphone _smartphone;
+    [SerializeField] public Image _indicatorImage;
 
-    //private void OnEnable()
-    //{
-    //    _smartphone.OnNewMessegeReceived += PlayNewMessegeIndicator;
-    //}
+    private Sequence _indicationSequence;
 
-    //private void OnDisable()
-    //{
-    //    _smartphone.OnNewMessegeReceived -= PlayNewMessegeIndicator;
-    //}
+    private void Awake()
+    {
+        _indicationSequence = DOTween.Sequence();
+    }
+
+    private void OnEnable()
+    {
+        _smartphone.OnNewMessegeReceived += PlayNewMessegeIndicator;
+    }
+
+    private void OnDisable()
+    {
+        _smartphone.OnNewMessegeReceived -= PlayNewMessegeIndicator;
+    }
 
     private void PlayNewMessegeIndicator()
     {
-
+        _indicationSequence
+            .Append(_indicatorImage.DOColor(new(1, 1, 1, 1), 1))
+            .Append(_indicatorImage.DOColor(new(1, 1, 1, 0), 1))
+            .SetLoops(-1)
+            .Play();
     }
 }
