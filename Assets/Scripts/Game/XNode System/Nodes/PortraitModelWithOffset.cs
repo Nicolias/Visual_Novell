9aa913514f1889fb32fd91e8e81b90a103b20242
Nicolias <marat.khakimov.04@gmail.com>
2023-10-26 11:25:59 +0500

    Закончил сюжет

---
+++ Assets/Scripts/Game/XNode System/Nodes/PortraitModelWithOffset.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class PortraitModelWithOffset : PortraitModel
+{
+    [Header(("Position Setting"))]
+    [field: SerializeField] public Vector2 PositionOffset { get; private set; }
+    [field: SerializeField] public Vector3 ScaleOffset { get; private set; }
+}
