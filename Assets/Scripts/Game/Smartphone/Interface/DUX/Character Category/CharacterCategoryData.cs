316717e1787bba5b480f766e082b7790d948f918
Nicolias <marat.khakimov.04@gmail.com>
2023-12-07 01:21:59 +0500

    Рандомная генерация вопросов викторины и блок дюкс лизы до встречи с ней

--- Assets/Scripts/Game/Smartphone/Interface/DUX/Character Category/CharacterCategoryData.cs
+++ Assets/Scripts/Game/Smartphone/Interface/DUX/Character Category/CharacterCategoryData.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿using Characters;
+using UnityEngine;
 
 [CreateAssetMenu(menuName = "Category/Character")]
 public class CharacterCategoryData : DUXCategoryData
 {
     [field: SerializeField] public Sprite CharacterSprite { get; private set; }
     [field: SerializeField] public CharacterType CharacterType { get; private set; }
+
+    [SerializeField] private int _levelLimit;
+
+    public sealed override bool IsBlocked(CharactersLibrary charactersLibrary)
+    {
+        ICharacter character = charactersLibrary.GetCharacter(CharacterType);
+
+        if (character.IsMeetingWithPlayer & character.SympathyLevel >= _levelLimit)
+            return false;
+        else
+            return true;
+    }
 
     public sealed override void Accept(IDUXVisitor visitor)
     {
