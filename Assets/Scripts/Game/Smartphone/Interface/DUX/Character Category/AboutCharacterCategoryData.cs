5a7bb6ae9384df0ad89d4efff6e9ccb5c2a13ef1
Nicolias <marat.khakimov.04@gmail.com>
2023-12-07 01:21:59 +0500

    Рандомная генерация вопросов викторины и блок дюкс лизы до встречи с ней

--- Assets/Scripts/Game/Smartphone/Interface/DUX/Character Category/AboutCharacterCategoryData.cs
+++ Assets/Scripts/Game/Smartphone/Interface/DUX/Character Category/AboutCharacterCategoryData.cs
@@ -1,10 +1,8 @@
-﻿using UnityEngine;
+﻿using Characters;
+using UnityEngine;
 
 [CreateAssetMenu(menuName = "Category/Character/Information")]
 public class AboutCharacterCategoryData : CharacterCategoryData
 {
-    public override void Accept(IDUXVisitor visitor)
-    {
-        visitor.Visit(this);
-    }
+    
 }
