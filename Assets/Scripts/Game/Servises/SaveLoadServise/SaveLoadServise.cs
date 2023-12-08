1f7aad8056db5696cdb548021610872dc0ed2e62
Nicolias <marat.khakimov.04@gmail.com>
2023-12-08 16:49:16 +0500

    Сделал систему регистрации и авторизации

--- Assets/Scripts/Game/Servises/SaveLoadServise/SaveLoadServise.cs
+++ Assets/Scripts/Game/Servises/SaveLoadServise/SaveLoadServise.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using UnityEngine;
+using Unity.Services.CloudSave;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 public class SaveLoadServise : MonoBehaviour
 {
+    private Dictionary<string, Unity.Services.CloudSave.Models.Item> _results;
+
+    private async void Awake()
+    {
+        _results = await CloudSaveService.Instance.Data.Player.LoadAllAsync();
+    }
+
     public void Save<T>(string key, T saveData)
     {
         string jsonDataString = JsonUtility.ToJson(saveData, true);
-        PlayerPrefs.SetString(key, jsonDataString);
+        ForceSaveObjectData(key, jsonDataString);
     }
 
     public T Load<T>(string key) where T : new()
     {
         if (HasSave(key))
         {
-            string loadedString = PlayerPrefs.GetString(key);
+            string loadedString = RetrieveSpecificData<string>(key);
             return JsonUtility.FromJson<T>(loadedString);
         }
         else
@@ -29,6 +39,73 @@ public class SaveLoadServise : MonoBehaviour
 
     public bool HasSave(string key)
     {
-        return PlayerPrefs.HasKey(key);
+        if (_results == null)
+            return false;
+
+        return _results.ContainsKey(key);
+    }
+
+    private async Task<string> ForceSaveObjectData(string key, string value)
+    {
+        try
+        {
+            // Although we are only saving a single value here, you can save multiple keys
+            // and values in a single batch.
+            Dictionary<string, object> oneElement = new Dictionary<string, object>
+                {
+                    { key, value }
+                };
+
+            // Saving data without write lock validation by passing the data as an object instead of a SaveItem
+            Dictionary<string, string> result = await CloudSaveService.Instance.Data.Player.SaveAsync(oneElement);
+            string writeLock = result[key];
+
+            Debug.Log($"Successfully saved {key}:{value} with updated write lock {writeLock}");
+
+            return writeLock;
+        }
+        catch (CloudSaveValidationException e)
+        {
+            Debug.LogError(e);
+        }
+        catch (CloudSaveRateLimitedException e)
+        {
+            Debug.LogError(e);
+        }
+        catch (CloudSaveException e)
+        {
+            Debug.LogError(e);
+        }
+
+        return null;
+    }
+
+    private T RetrieveSpecificData<T>(string key)
+    {
+        try
+        {
+            if (_results.TryGetValue(key, out var item))
+            {
+                return item.Value.GetAs<T>();
+            }
+            else
+            {
+                Debug.Log($"There is no such key as {key}!");
+            }
+        }
+        catch (CloudSaveValidationException e)
+        {
+            Debug.LogError(e);
+        }
+        catch (CloudSaveRateLimitedException e)
+        {
+            Debug.LogError(e);
+        }
+        catch (CloudSaveException e)
+        {
+            Debug.LogError(e);
+        }
+
+        return default;
     }
 }
